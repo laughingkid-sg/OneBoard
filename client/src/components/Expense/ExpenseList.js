a4c89e498e029813d4f40e0bfe5d632694f0692b
KwanHW <kwanhaowei@gmail.com>
2021-07-07 23:14:35 +0800

    Added filter function to expenses, expect refactor

--- client/src/components/Expense/ExpenseList.js
+++ client/src/components/Expense/ExpenseList.js
@@ -1,86 +1,21 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Button, Input, Label } from 'reactstrap';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import ExpenseTable from './ExpenseTable';
-import useInput from '../hooks/use-input';
-import { textNotEmpty } from '../../lib/validators';
-
-function queryInString(value, query) {
-	return value.toLowerCase().includes(query.toLowerCase());
-}
+import { useSelector } from 'react-redux';
+import FilterExpense from './FilterExpense';
 
 function ExpenseList(props) {
-	const expenses = useSelector((state) => state.expense);
-	const [checked, setChecked] = useState({ name: true, description: true });
-	const [filteredExpenses, setFilteredExpenses] = useState(expenses.expense);
-	const {
-		value: filter,
-		isValid: filterIsValid,
-		onChange: filterOnChange,
-		reset: filterReset,
-	} = useInput(textNotEmpty, '');
-	const nameRef = useRef();
-	const descriptionRef = useRef();
-
-	useEffect(() => {
-		// console.log(checked);
-		const filtered = filterIsValid
-			? expenses.expense.filter(
-					(expense) =>
-						(checked.name && queryInString(expense.name, filter)) ||
-						(checked.description &&
-							queryInString(expense.description, filter))
-			  )
-			: expenses.expense;
-		// if (filtered.length < expenses.expense.length) console.log(filtered);
-		setFilteredExpenses(filtered);
-		return () => {};
-	}, [filter, checked, expenses.expense]);
+	const { expense, labels } = useSelector((state) => state.expense);
+	const [filteredExpenses, setFilteredExpenses] = useState([]);
 
 	return (
 		<div>
-			{/* <h3>Transaction History</h3> */}
-			{/* TODO - Filter expenses */}
-			{/* TODO - Include Date Range */}
-			<div className="d-flex align-items-center">
-				<div className="d-flex w-75">
-					<Input
-						id="filter"
-						name="filter"
-						placeholder="Enter Transaction Name"
-						value={filter}
-						onChange={filterOnChange}
-					/>
-					<Button onClick={filterReset}>Clear</Button>
-				</div>
+			{/* For now written like this, will be generalised to kanban */}
+			<FilterExpense
+				allExpenses={expense}
+				filterResults={setFilteredExpenses}
+				labelSrc={labels}
+			/>
 
-				<Input
-					id="name"
-					name="name"
-					type="checkbox"
-					innerRef={nameRef}
-					checked={checked.name}
-					onChange={() => {
-						setChecked({ ...checked, name: !checked.name });
-					}}
-				/>
-				<Label for="name">Name</Label>
-
-				<Input
-					id="description"
-					name="description"
-					type="checkbox"
-					innerRef={descriptionRef}
-					checked={checked.description}
-					onChange={() => {
-						setChecked({
-							...checked,
-							description: !checked.description,
-						});
-					}}
-				/>
-				<Label for="description">Description</Label>
-			</div>
 			<ExpenseTable expenses={filteredExpenses} />
 		</div>
 	);
---
+++ client/src/components/Expense/FilterExpense.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Button, Input, Label } from 'reactstrap';
+import useInput from '../hooks/use-input';
+import { hasId, textNotEmpty } from '../../lib/validators';
+import Dropdown from '../../UI/Dropdown/Dropdown';
+
+function queryInString(value, query) {
+	return value.toLowerCase().includes(query.toLowerCase());
+}
+
+function FilterExpense(props) {
+	const { allExpenses, filterResults } = props;
+	const labels = props.labelSrc.filter(hasId);
+	const {
+		value: filter,
+		isValid: filterIsValid,
+		onChange: filterOnChange,
+		reset: filterReset,
+	} = useInput(textNotEmpty, '');
+	const [checked, setChecked] = useState({ name: true, description: true });
+	const [labelSelect, setLabelSelect] = useState([]);
+	const nameRef = useRef();
+	const descriptionRef = useRef();
+
+	{
+		/* TODO - Include Date Range */
+	}
+	useEffect(() => {
+		function filterExpenses() {
+			let filteredExp =
+				labelSelect.length === 0
+					? allExpenses
+					: allExpenses.filter((expense) =>
+							expense.label.some((id) => labelSelect.includes(id))
+					  );
+
+			if (filterIsValid) {
+				filteredExp = filteredExp.filter(
+					(expense) =>
+						(checked.name && queryInString(expense.name, filter)) ||
+						(checked.description &&
+							queryInString(expense.description, filter))
+				);
+			}
+			return filteredExp;
+		}
+
+		filterResults(filterExpenses());
+		return () => {};
+	}, [filter, checked, allExpenses, labelSelect.length]);
+
+	return (
+		<div className="d-flex w-100 align-items-center">
+			<Input
+				id="filter"
+				name="filter"
+				placeholder="Enter Transaction Name"
+				value={filter}
+				onChange={filterOnChange}
+				className="w-25"
+			/>
+			<div className="mx-2">
+				<Input
+					id="filterName"
+					name="filterName"
+					type="checkbox"
+					innerRef={nameRef}
+					checked={checked.name}
+					onChange={() => {
+						setChecked({ ...checked, name: !checked.name });
+					}}
+				/>
+				<Label for="filterName" style={{ marginRight: '4px' }}>
+					Name
+				</Label>
+
+				<Input
+					id="description"
+					name="description"
+					type="checkbox"
+					innerRef={descriptionRef}
+					checked={checked.description}
+					onChange={() => {
+						setChecked({
+							...checked,
+							description: !checked.description,
+						});
+					}}
+				/>
+				<Label for="description">Description</Label>
+			</div>
+			<Dropdown
+				value={labelSelect}
+				onChange={(value) => {
+					setLabelSelect(value);
+				}}
+				className="w-25"
+				labelSrc={labels}
+			/>
+			<Button onClick={filterReset} className="mx-2">
+				Reset
+			</Button>
+		</div>
+	);
+}
+
+export default FilterExpense;
